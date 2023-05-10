d40a350f61a3affb5c00fda536933e6e25b89e2c
Reznorzo <skgedtxm@anonaddy.me>
2023-05-10 15:51:58 +0100

    Added zsh config files

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first --long --header --all"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
+
+alias cbp="code /home/xcad/obsidianvault/boilerplates"
+alias cpr="code /home/xcad/obsidianvault/projects"
+
+# Scripts
+alias obsidian="python3 /Users/xcad/Projects/christianlempa/scripts/obsidian"
+
+# Dotfiles Bare Repo
+alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
